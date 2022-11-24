bdff081d3c6c859741a5b82621d057c64059603c
Fatih Cirakoglu <fatihcirak@gmail.com>
2022-11-25 02:58:54 +0300

    feat: added models for user credentials and profile page

--- src/iotapp/models.py
+++ src/iotapp/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils.text import slugify
+from django.db.models.signals import pre_save,post_save
+from .utils import get_read_time
+from django.urls import reverse
+from django.dispatch import receiver
+
 
 # Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    
+    profile_image = models.ImageField(default='default.jpeg', upload_to ='profile_pics', null=True, blank=True)
+
+    def __str__(self):
+        return '%s %s' % (self.user.first_name, self.user.last_name)
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
+
+
