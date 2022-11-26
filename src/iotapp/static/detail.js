08e784f4fb071d24413117f1c0459eecda90be03
Fatih Cirakoglu <fatihcirak@gmail.com>
2022-11-26 04:07:42 +0300

    feat: changed css settings

---
+++ src/iotapp/static/detail.js
@@ -0,0 +1,26 @@
+function toggle() {
+  let bts = document.getElementsByClassName("button");
+  for (let btn of bts) {
+    if (btn.classList.contains("button-green")) {
+      btn.classList.remove("button-green");
+      btn.classList.add("button-red");
+      btn.innerHTML = "Quit Space";
+    } else {
+      btn.classList.remove("button-red");
+      btn.classList.add("button-green");
+      btn.innerHTML = "Join Space";
+    }
+  }
+}
+
+$(".button").click(function () {
+  var slug;
+  slug = $(this).attr("data-slug");
+  $.ajax({
+    type: "GET",
+    url: `/devicedetail/${slug}/Favourites`,
+    success: function (data) {
+      console.log("Success");
+    },
+  });
+});
