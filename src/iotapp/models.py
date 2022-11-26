83ce847f332e217b09888eb679e217890b2e3e24
Fatih Cirakoglu <fatihcirak@gmail.com>
2022-11-26 03:40:06 +0300

    feat: created db model -Added Device, Favourite device and snap models -Snap is list of snapd applications in device

--- src/iotapp/models.py
+++ src/iotapp/models.py
@@ -4,11 +4,18 @@ from django.utils.text import slugify
 from django.db.models.signals import pre_save,post_save
 from .utils import get_read_time
 from django.urls import reverse
+from taggit.managers import TaggableManager
 from django.dispatch import receiver
+from ckeditor.fields import RichTextField
 
 
 # Create your models here.
+class TagDict(models.Model):
+    tag = models.CharField(max_length=100)
+    count = models.IntegerField(default=0)
 
+    def __str__(self):
+        return self.tag
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     
@@ -17,6 +24,62 @@ class Profile(models.Model):
     def __str__(self):
         return '%s %s' % (self.user.first_name, self.user.last_name)
 
+class Device(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True, editable=False)
+    idnum = models.IntegerField(default=0, editable=False)
+    brand = models.CharField(max_length=200, unique=True, editable=False)
+    model = models.CharField(max_length=200, unique=True, editable=False)
+    serialno = models.IntegerField(default=0, editable=False)
+    devicekey = models.IntegerField(default=0, editable=False)
+    status = models.IntegerField(default=0, editable=False)
+    author = models.ForeignKey(User, on_delete= models.CASCADE)
+    updated_on = models.DateTimeField(auto_now= True)
+    content = RichTextField(blank=True, null=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+    image = models.ImageField(null=True, blank=True, upload_to='images/')
+    tags = TaggableManager(blank=True)
+
+    class Meta:
+        ordering = ['-created_on']
+
+    def __str__(self):
+        return self.title
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title, allow_unicode=True)
+        super().save(*args, **kwargs)
+
+        for tag in self.tags.all():
+            tag_dict,_ = TagDict.objects.get_or_create(tag=str(tag))
+            tag_dict.count += 1
+            tag_dict.save()
+
+    def get_absolute_url(self):
+        return reverse('device_detail', kwargs={"slug":self.slug})
+
+def pre_save_device_receiver(sender, instance, *args, **kwargs):
+    if instance.content:
+        instance.read_time = get_read_time(instance.content)
+
+pre_save.connect(pre_save_device_receiver, sender=Device)
+
+
+class FavouriteDevice(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    devices = models.ManyToManyField(Device)
+
+class Snap(models.Model):
+    device = models.ForeignKey(Device,on_delete=models.CASCADE,related_name='snaps')
+    name = models.CharField(max_length=80)
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        ordering = ['created_on']
+
+    def __str__(self):
+        return 'Snap {} by {}'.format(self.body, self.name)
 
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
