a2a05e18874651bbe80086fba9fb95a580c7c11a
Fatih Cirakoglu <fatihcirak@gmail.com>
2022-11-26 04:10:58 +0300

    fix: added db models in admin to fix db creation problem

--- src/iotapp/admin.py
+++ src/iotapp/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Device, FavouriteDevice, Profile, Snap, TagDict
 
 # Register your models here.
+admin.site.register(Device)
+admin.site.register(FavouriteDevice)
+admin.site.register(Profile)
+admin.site.register(Snap)
+admin.site.register(TagDict)
